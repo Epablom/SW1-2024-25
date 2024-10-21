96de0b024865c392e2d3136f8e73c664df245c2a
Carlos <c.marcos3@usp.ceu.es>
2024-10-21 17:13:50 +0200

    Creada la página de noticias y otros cambios

--- src/public/js/script.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementById('toggle-button');
-const sidebar = document.getElementById('sidebar');
-
-toggleButton.addEventListener('click', function() {
-  sidebar.classList.toggle('open');
-  toggleButton.classList.toggle('open');
-  toggleButton.textContent = toggleButton.classList.contains('open') ? '✖' : '☰';
-});
