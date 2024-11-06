241b5a384d0048f2f9b137770cc52ae0d023eb36
Carlos <c.marcos3@usp.ceu.es>
2024-11-06 14:13:08 +0100

    Añadida propuesta de dashboard

---
+++ src/public/js/alertas.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        userName: 'Jack',
+        publication: ['#', 'SWEB1'],
+        tiquetNum: '0001',
+        status: 'Pending'
+    },
+    {
+        userName: 'Paul',
+        publication: ['#', 'SIPD'],
+        tiquetNum: '0002',
+        status: 'Declined'
+    },
+    {
+        userName: 'Jack',
+        publication: ['#', 'ISO'],
+        tiquetNum: '0003',
+        status: 'Active'
+    },
+]
